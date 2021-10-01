08cefea480eb1bc87afee9d2d1ee7c9ae4d88a4e
Vinit Raj <vinit2001raj@outlook.com>
2021-10-01 12:53:22 +0530

    Added two more new language

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -3,78 +3,82 @@ import Dropdown from "./Dropdown";
 import Convert from "./Convert";
 
 const options = [
-    { 
-        label:'Afrikaans',
-        value:'af'
-    },
-    {
-        label:'Albanian',
-        value:'sq'
-    },
-    {
-        label: 'Bengali',
-        value: 'bn'
-    },
-    {
-        label: 'English',
-        value: 'en'
-    },
-    {
-        label: 'French',
-        value: 'fr'
-    },
-    {
-        label: 'German',
-        value: 'de'
-    },
-    {
-        label: 'Hindi',
-        value: 'hi'
-    },
-    {
-        label: 'Italian',
-        value: 'it'
-    },
-    {
-        label: 'Punjabi',
-        value: 'pa'
-    },
-    {
-        label: 'Telugu',
-        value: 'te'
-    },
-]
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Albanian",
+    value: "sq",
+  },
+  {
+    label: "Bengali",
+    value: "bn",
+  },
+  {
+    label: "English",
+    value: "en",
+  },
+  {
+    label: "French",
+    value: "fr",
+  },
+  {
+    label: "German",
+    value: "de",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+  {
+    label: "Italian",
+    value: "it",
+  },
+  {
+    label: "Punjabi",
+    value: "pa",
+  },
+  {
+    label: "Telugu",
+    value: "te",
+  },
+  {
+    label: "Korean",
+    value: "ko",
+  },
+  {
+    label: "Sindhi",
+    value: "sd",
+  },
+];
 
 const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
 
-    const [language, setLanguage] = useState(options[0]);
-    const [text, setText] = useState('');
-
-    return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <h4><label>Enter text</label></h4>
-                    <input
-                        value={text}
-                        onChange={
-                            (e) => setText(e.target.value)
-                        }
-                    />
-                </div>
-            </div>
-            <Dropdown
-                options={options}
-                selected={language}
-                onSelectedChange={setLanguage}
-                label="Select a Language"
-                demo=""
-            />
-            
-            <h3 className="ui header">Output</h3>
-            <Convert text={text} language={language}/>
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <h4>
+            <label>Enter text</label>
+          </h4>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
         </div>
-    );
+      </div>
+      <Dropdown
+        options={options}
+        selected={language}
+        onSelectedChange={setLanguage}
+        label="Select a Language"
+        demo=""
+      />
+
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
+    </div>
+  );
 };
 
 export default Translate;
