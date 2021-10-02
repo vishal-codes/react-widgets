20631a5e8307fa955fb0a8016e43fa1e79c65f60
Vishal Shinde <itsvishal2417@gmail.com>
2021-10-02 19:50:16 +0530

    Refactored Project Structure

--- src/data/translateOptions.js
+++ src/data/translateOptions.js
@@ -1,8 +1,4 @@
-import React, { useState } from "react";
-import Dropdown from "./Dropdown";
-import Convert from "./Convert";
-
-const options = [
+export const translateOptions = [
     { 
         label: 'Afrikaans',
         value: 'af'
@@ -152,34 +148,3 @@ const options = [
         value: 'zu'
     },
 ]
-
-
-const Translate = () => {
-  const [language, setLanguage] = useState(options[0]);
-  const [text, setText] = useState("");
-
-  return (
-    <div>
-      <div className="ui form">
-        <div className="field">
-          <h4>
-            <label>Enter text</label>
-          </h4>
-          <input value={text} onChange={(e) => setText(e.target.value)} />
-        </div>
-      </div>
-      <Dropdown
-        options={options}
-        selected={language}
-        onSelectedChange={setLanguage}
-        label="Select a Language"
-        demo=""
-      />
-
-      <h3 className="ui header">Output</h3>
-      <Convert text={text} language={language} />
-    </div>
-  );
-};
-
-export default Translate;
