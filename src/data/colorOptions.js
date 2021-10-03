88ed4967684f95a3f339d8623d746b5a6334c538
Vishal Shinde <itsvishal2417@gmail.com>
2021-10-03 12:44:47 +0530

    Update colorOptions.js

--- src/data/colorOptions.js
+++ src/data/colorOptions.js
@@ -1,102 +1,146 @@
 export const colorOptions = [
   {
-    label: 'AntiqueWhite',
-    value: 'AntiqueWhite',
-  },
-  {
-    label: 'Aqua',
-    value: 'Aqua',
-  },
-  {
-    label: 'Aquamarine',
-    value: 'Aquamarine',
-  },
-  {
-    label: 'Blue',
-    value: 'Blue',
-  },
-  {
-    label: 'BlueViolet',
-    value: 'BlueViolet',
-  },
-  {
-    label: 'Brown',
-    value: 'Brown',
-  },
-  {
-    label: 'CadetBlue',
-    value: 'CadetBlue',
-  },
-  {
-    label: 'Charteuse',
-    value: 'Charteuse',
-  },
-  {
-    label: 'Coral',
-    value: 'Coral',
-  },
-  {
-    label: 'DarkBlue',
-    value: 'DarkBlue',
-  },
-  {
-    label: 'DarkRed',
-    value: 'Darkred',
-  },
-  {
-    label: 'DeepPink',
-    value: 'DeepPink',
-  },
-  {
-    label: 'Gold',
-    value: 'Gold',
-  },
-  {
-    label: 'Green',
-    value: 'Green',
-  },
-  {
-    label: 'Maroon',
-    value: 'Maroon',
-  },
-  {
-    label: 'Olive',
-    value: 'Olive',
-  },
-  {
-    label: 'OrangeRed',
-    value: 'OrangeRed',
-  },
-  {
-    label: 'Orchid',
-    value: 'Orchid',
-  },
-  {
-    label: 'Pink',
-    value: 'Pink',
-  },
-  {
-    label: 'Purple',
-    value: 'Purple',
-  },
-  {
-    label: 'Red',
-    value: 'Red',
-  },
-  {
-    label: 'Yellow',
-    value: 'Yellow',
-  },
-  {
-    label: 'DimGray',
-    value: 'DimGray',
-  },
-  {
-    label: 'Fuchsia',
-    value: 'Fuchsia',
-  },
-  {
-    label: 'Lime',
-    value: 'Lime',
-  },
+        label: 'AntiqueWhite',
+        value: 'AntiqueWhite'
+    },
+    {
+        label: 'Aqua',
+        value: 'Aqua'
+    },
+    {
+        label: 'Aquamarine',
+        value: 'Aquamarine'
+    },
+    {
+        label: 'Blue',
+        value: 'Blue'
+    },
+    {
+        label: 'BlueViolet',
+        value: 'BlueViolet'
+    },
+    {
+        label: 'Brown',
+        value: 'Brown'
+    },
+    {
+        label: 'CadetBlue',
+        value: 'CadetBlue'
+    },
+    {
+        label: 'Chocolate',
+        value: 'Chocolate'
+    },
+    {
+        label: 'Chartreuse',
+        value: 'Chartreuse'
+    },
+    {
+        label: 'Coral',
+        value: 'Coral'
+    },
+    {
+        label: 'Crimson',
+        value: 'Crimson'
+    },
+    {
+        label: 'Cyan',
+        value: 'Cyan'
+    },
+    {
+        label: 'DarkBlue',
+        value: 'DarkBlue'
+    },
+    {
+        label: 'DarkCyan',
+        value: 'DarkCyan'
+    },
+    {
+        label: 'DarkGoldenRed',
+        value: 'DarkGoldenRed'
+    },
+    {
+        label: 'DarkGray',
+        value: 'DarkGray'
+    },
+    {
+        label: 'DarkGreen',
+        value: 'DarkGreen'
+    },
+    {
+        label: 'DarkRed',
+        value: 'Darkred'
+    },
+    {
+        label: 'DarkViolet',
+        value: 'DarkViolet'
+    },
+    {
+        label: 'DeepPink',
+        value: 'DeepPink'
+    },
+    {
+        label: 'DimGray',
+        value: 'DimGray'
+    },
+    {
+        label: 'Fuchsia',
+        value: 'Fuchsia'
+    },
+    {
+        label: 'Gold',
+        value: 'Gold'
+    },
+    {
+        label: 'Green',
+        value: 'Green'
+    },
+    {
+        label: 'LightSalmon',
+        value: 'LightSalmon'
+    },
+    {
+        label: 'Maroon',
+        value: 'Maroon'
+    },
+    {
+        label: 'LightSeaGreen',
+        value: 'LightSeaGreen'
+    },
+    {
+        label: 'Lime',
+        value: 'Lime'
+    },
+    {
+        label: 'Olive',
+        value: 'Olive'
+    },
+    {
+        label: 'OrangeRed',
+        value: 'OrangeRed'
+    },
+    {
+        label: 'Orchid',
+        value: 'Orchid'
+    },
+    {
+        label: 'Pink',
+        value: 'Pink'
+    },
+    {
+        label: 'Purple',
+        value: 'Purple'
+    },
+    {
+        label: 'Red',
+        value: 'Red'
+    },
+    {
+        label: 'Violet',
+        value: 'Violet'
+    },
+    {
+        label: 'Yellow',
+        value: 'Yellow'
+    },
 ];
