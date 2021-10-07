de6b931ca5681d27a52252e280b46f3f793499cf
Altaf Ali <nerdyaltaf.official@gmail.com>
2021-10-07 13:07:52 +0530

    DodgerBlue and ForestGreen Color added

--- src/data/colorOptions.js
+++ src/data/colorOptions.js
@@ -1,218 +1,226 @@
 export const colorOptions = [
   {
-      label: 'AntiqueWhite',
-      value: 'AntiqueWhite'
+    label: "AntiqueWhite",
+    value: "AntiqueWhite",
   },
   {
-      label: 'Aqua',
-      value: 'Aqua'
+    label: "Aqua",
+    value: "Aqua",
   },
   {
-      label: 'Aquamarine',
-      value: 'Aquamarine'
+    label: "Aquamarine",
+    value: "Aquamarine",
   },
   {
-      label: 'Blue',
-      value: 'Blue'
-  }, 
-  { 
-      label: 'BlueViolet',
-      value: 'BlueViolet'
+    label: "Blue",
+    value: "Blue",
   },
   {
-      label: 'Brown',
-      value: 'Brown'
-  }, 
-  { 
-      label: 'CadetBlue',
-      value: 'CadetBlue'
-  }, 
-  { 
-      label: 'Chartreuse',
-      value: 'Chartreuse'
-  }, 
-  { 
-      label: 'Chocolate',
-      value: 'Chocolate'
-  },  
-  {  
-      label: 'Coral',
-      value: 'Coral'
-  },  
-  {  
-      label: 'CornflowerBlue',
-      value: 'CornflowerBlue'
-  },  
-  {  
-      label: 'Crimson',
-      value: 'Crimson'
-  },  
-  {  
-      label: 'Cyan',
-      value: 'Cyan'
-  },  
-  {  
-      label: 'DarkBlue',
-      value: 'DarkBlue'
-  },  
-  {  
-      label: 'DarkCyan',
-      value: 'DarkCyan'
+    label: "BlueViolet",
+    value: "BlueViolet",
   },
   {
-      label: 'DarkOliveGreen',
-      value: 'DarkOliveGreen'
-  },  
-  {  
-      label: 'DarkOrange',
-      value: 'DarkOrange'
-  },  
-  {  
-      label: 'DarkGray',
-      value: 'DarkGray'
-  },  
-  {  
-      label: 'DarkGreen',
-      value: 'DarkGreen'
-  },  
-  {  
-      label: 'DarkKhaki',
-      value: 'DarkKhaki'
-  },  
-  {  
-      label: 'DarkMagenta',
-      value: 'DarkMagenta'
+    label: "Brown",
+    value: "Brown",
   },
   {
-      label: 'DarkOrchid',
-      value: 'DarkOrchid'
-  },  
-  {
-      label: 'DarkRed',
-      value: 'Darkred'
-  },  
-  {
-      label: 'DarkTurquoise',
-      value: 'DarkTurquoise'
-  },  
-  {  
-      label: 'DarkViolet',
-      value: 'DarkViolet'
-  },  
-  {  
-      label: 'DeepPink',
-      value: 'DeepPink'
-  },  
-  {  
-      label: 'DimGray',
-      value: 'DimGray'
-  },  
-  {  
-      label: 'Fuchsia',
-      value: 'Fuchsia'
-  },  
-  {  
-      label: 'Gold',
-      value: 'Gold'
-  },  
-  {  
-      label: 'Green',
-      value: 'Green'
-  },  
-  {  
-      label: 'HotPink',
-      value: 'HotPink'
+    label: "CadetBlue",
+    value: "CadetBlue",
   },
   {
-      label: 'Khaki',
-      value: 'Khaki'
+    label: "Chartreuse",
+    value: "Chartreuse",
   },
   {
-      label: 'Lavender',
-      value: 'Lavender'
-  },
-  {  
-      label: 'LightBlue',
-      value: 'LightBlue'
+    label: "Chocolate",
+    value: "Chocolate",
   },
   {
-      label: 'LightCoral',
-      value: 'LightCoral'
+    label: "Coral",
+    value: "Coral",
   },
   {
-    label: 'LightGreen',
-    value: 'LightGreen'
-},  
-  {
-      label: 'LightPink',
-      value: 'LightPink'
-  },
-  {  
-      label: 'LightSalmon',
-      value: 'LightSalmon'
-  },  
-  {  
-      label: 'LightSeaGreen',
-      value: 'LightSeaGreen'
-  },  
-  {  
-      label: 'Lime',
-      value: 'Lime'
-  },
-  {  
-      label: 'Linen',
-      value: 'Linen'
-  }, 
-  {  
-      label: 'Maroon',
-      value: 'Maroon'
-  },  
-  {  
-      label: 'NavajoWhite',
-      value: 'NavajoWhite'
-  },  
-  {  
-      label: 'Olive',
-      value: 'Olive'
-  },  
-  {  
-      label: 'OrangeRed',
-      value: 'OrangeRed'
-  },  
-  {  
-      label: 'Orchid',
-      value: 'Orchid'
-  },  
-  {  
-      label: 'PaleVioletRed',
-      value: 'PaleVioletRed'
+    label: "CornflowerBlue",
+    value: "CornflowerBlue",
   },
   {
-      label: 'Pink',
-      value: 'Pink'
-  },  
-  {  
-      label: 'Peru',
-      value: 'Peru'
-  },  
-  {  
-      label: 'Purple',
-      value: 'Purple'
-  },  
-  {  
-      label: 'Red',
-      value: 'Red'
-  },  
-  {  
-      label: 'SandyBrown',
-      value: 'SandyBrown'
-  },  
-  {  
-      label: 'Violet',
-      value: 'Violet'
-  },  
-  {  
-      label: 'Yellow',
-      value: 'Yellow'
+    label: "Crimson",
+    value: "Crimson",
+  },
+  {
+    label: "Cyan",
+    value: "Cyan",
+  },
+  {
+    label: "DarkBlue",
+    value: "DarkBlue",
+  },
+  {
+    label: "DarkCyan",
+    value: "DarkCyan",
+  },
+  {
+    label: "DarkOliveGreen",
+    value: "DarkOliveGreen",
+  },
+  {
+    label: "DarkOrange",
+    value: "DarkOrange",
+  },
+  {
+    label: "DarkGray",
+    value: "DarkGray",
+  },
+  {
+    label: "DarkGreen",
+    value: "DarkGreen",
+  },
+  {
+    label: "DarkKhaki",
+    value: "DarkKhaki",
+  },
+  {
+    label: "DarkMagenta",
+    value: "DarkMagenta",
+  },
+  {
+    label: "DarkOrchid",
+    value: "DarkOrchid",
+  },
+  {
+    label: "DarkRed",
+    value: "Darkred",
+  },
+  {
+    label: "DarkTurquoise",
+    value: "DarkTurquoise",
+  },
+  {
+    label: "DarkViolet",
+    value: "DarkViolet",
+  },
+  {
+    label: "DeepPink",
+    value: "DeepPink",
+  },
+  {
+    label: "DimGray",
+    value: "DimGray",
+  },
+  {
+    label: "DodgerBlue",
+    value: "DodgerBlue",
+  },
+  {
+    label: "ForestGreen",
+    value: "ForestGreen",
+  },
+  {
+    label: "Fuchsia",
+    value: "Fuchsia",
+  },
+  {
+    label: "Gold",
+    value: "Gold",
+  },
+  {
+    label: "Green",
+    value: "Green",
+  },
+  {
+    label: "HotPink",
+    value: "HotPink",
+  },
+  {
+    label: "Khaki",
+    value: "Khaki",
+  },
+  {
+    label: "Lavender",
+    value: "Lavender",
+  },
+  {
+    label: "LightBlue",
+    value: "LightBlue",
+  },
+  {
+    label: "LightCoral",
+    value: "LightCoral",
+  },
+  {
+    label: "LightGreen",
+    value: "LightGreen",
+  },
+  {
+    label: "LightPink",
+    value: "LightPink",
+  },
+  {
+    label: "LightSalmon",
+    value: "LightSalmon",
+  },
+  {
+    label: "LightSeaGreen",
+    value: "LightSeaGreen",
+  },
+  {
+    label: "Lime",
+    value: "Lime",
+  },
+  {
+    label: "Linen",
+    value: "Linen",
+  },
+  {
+    label: "Maroon",
+    value: "Maroon",
+  },
+  {
+    label: "NavajoWhite",
+    value: "NavajoWhite",
+  },
+  {
+    label: "Olive",
+    value: "Olive",
+  },
+  {
+    label: "OrangeRed",
+    value: "OrangeRed",
+  },
+  {
+    label: "Orchid",
+    value: "Orchid",
+  },
+  {
+    label: "PaleVioletRed",
+    value: "PaleVioletRed",
+  },
+  {
+    label: "Pink",
+    value: "Pink",
+  },
+  {
+    label: "Peru",
+    value: "Peru",
+  },
+  {
+    label: "Purple",
+    value: "Purple",
+  },
+  {
+    label: "Red",
+    value: "Red",
+  },
+  {
+    label: "SandyBrown",
+    value: "SandyBrown",
+  },
+  {
+    label: "Violet",
+    value: "Violet",
+  },
+  {
+    label: "Yellow",
+    value: "Yellow",
   },
 ];
